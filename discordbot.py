d838aedf205144c6c9c3fbf76a062e9a48722b41
Polyotu <55525642+Polyotu@users.noreply.github.com>
2021-07-18 22:24:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ import traceback
 import numpy as np
 import cv2
 
-bot = commands.Bot(command_prefix='///')
+bot = commands.Bot(command_prefix='%dip')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 blankPicture=np.zeros([400, 800, 3])
